4c186288d4a9ca8cb5f20241014f72a615a6251f
Daniel <daniel.maitre@durham.ac.uk>
2017-09-26 16:17:40 +0100

    fixed bug in the cachedfunction implementation

--- cachedFunction.cpp
+++ cachedFunction.cpp
@@ -7,6 +7,8 @@
 
 
 #include "cachedFunction.h"
+#include "debug.h"
+
 
 template <class argT> bool cache<argT>::get(const argT& args,double& value){
 	typename std::map<argT,double>::iterator it = d_store.find(args);
@@ -20,12 +22,15 @@ template <class argT> bool cache<argT>::get(const argT& args,double& value){
 
 
 template <class argT> void cache<argT>::set(const argT& args,double value){
+	NAMED_DEBUG("CACHE_STATS", std::cout << "cache args size!" << d_args.size() << std::endl;  )
+	NAMED_DEBUG("CACHE_STATS", std::cout << "cache storage size!" << d_store.size() << std::endl;  )
 	if ( d_args.size()==d_maxSize ){
 		typename std::map<argT,double>::iterator it = d_store.find(d_args.back());
 		d_store.erase (it);
 		d_args.pop_back();
 	}
 	d_store[args]=value;
+	d_args.push_front(args);
 }
 
 bool operator<(const args_t& a1,const args_t& a2){
